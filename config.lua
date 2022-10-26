80da09158b76c7b240f3fe84fc5d24a12e76f8df
Simon <770632+Ponjimon@users.noreply.github.com>
2022-10-26 16:54:21 +0000

    chore: update default config

--- config.lua
+++ config.lua
@@ -1,8 +1,8 @@
 SaltyRange = {}
-SaltyRange.ShowRangeDuration = 1
+SaltyRange.ShowRangeDuration = 2
 SaltyRange.RangeColor = {
     ['R'] = 0, -- Red 0-255
-    ['G'] = 255, -- Green 0-255
-    ['B'] = 0, -- Blue 0-255
-    ['A'] = 255 -- Alpha (Opacity) 0-255
+    ['G'] = 100, -- Green 0-255
+    ['B'] = 100, -- Blue 0-255
+    ['A'] = 80 -- Alpha (Opacity) 0-255
 }
