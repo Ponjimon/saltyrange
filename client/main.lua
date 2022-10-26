07514b7b3b10f26be18062aaf0d46371b7557e68
Simon <770632+Ponjimon@users.noreply.github.com>
2022-10-26 16:54:31 +0000

    fix: use cylinder marker

--- client/main.lua
+++ client/main.lua
@@ -12,7 +12,7 @@ AddEventHandler('SaltyChat_VoiceRangeChanged', function()
             local VoiceRange = exports['saltychat']:GetVoiceRange()
             local CloudTime = GetCloudTimeAsInt();
             
-            DrawMarker(25, PlayerPos.x, PlayerPos.y, PlayerPos.z - 0.95, 0, 0, 0, 0, 0, 0, VoiceRange * 2, VoiceRange * 2, VoiceRange,  SaltyRange.RangeColor.R, SaltyRange.RangeColor.G, SaltyRange.RangeColor.B, SaltyRange.RangeColor.A, false, false, 2, false, nil, nil, false)
+            DrawMarker(1, PlayerPos.x, PlayerPos.y, PlayerPos.z - 0.95, 0, 0, 0, 0, 0, 0, VoiceRange * 2, VoiceRange * 2, VoiceRange,  SaltyRange.RangeColor.R, SaltyRange.RangeColor.G, SaltyRange.RangeColor.B, SaltyRange.RangeColor.A, false, false, 2, false, nil, nil, false)
             
             if CloudTime >= HideAt then
                 ShowVoiceRange = false
